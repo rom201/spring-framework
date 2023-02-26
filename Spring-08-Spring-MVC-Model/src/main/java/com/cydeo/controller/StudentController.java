45cec4a62765bd2b21fe1fd370a8585a540e847c
rom201 <kovalchukr1104@gmail.com>
2023-02-26 16:11:19 -0500

    mentor task completed

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -12,6 +17,27 @@ public class StudentController {
 
         model.addAttribute("name", "Cydeo");   // this is belong only this page method
         model.addAttribute("course", "MVC");  // can bring this from DB or any
+
+        String subject = "Collections";  //can br DB
+        model.addAttribute("subject",subject);
+
+        int studentId = new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
+
+
+
+
+
         return "student/welcome";  //this is thymeleaf template
     }
 
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/model/Mentor.java
@@ -0,0 +1,19 @@
+package com.cydeo.model;
+
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class Mentor {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    private Gender gender;
+
+}
