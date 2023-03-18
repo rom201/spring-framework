307a396a2a99166072dc6b0f6baff6834e753991
rom201 <kovalchukr1104@gmail.com>
2023-03-18 13:54:33 -0400

    redirect and post method was completed

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -19,10 +21,17 @@ public class MentorController {
         model.addAttribute("mentor", new Mentor());
         List<String> batchList = Arrays.asList("JD1", "JD2", "UA1","UA2");
         model.addAttribute("batchList", batchList);
-
-
-
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor){
+
+        //test
+        System.out.println(mentor.toString()); //do with this data
+
+//        return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 
 
