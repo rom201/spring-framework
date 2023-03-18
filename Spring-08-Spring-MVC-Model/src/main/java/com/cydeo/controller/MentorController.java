3c8ee97dc56f9608b9f59e3ce12b4f441047f872
rom201 <kovalchukr1104@gmail.com>
2023-03-18 10:22:54 -0400

    week 8 iteration for mentor was added

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -19,7 +19,7 @@ public class MentorController {
         List<Mentor> mentorList = new ArrayList<>();
         mentorList.add(new Mentor("Tom","Hanks", 64, Gender.MALE));
         mentorList.add(new Mentor("Mike","Smith", 45, Gender.MALE));
-        mentorList.add(new Mentor("Ammy","Bryan", 25, Gender.FEMALE));
+        mentorList.add(new Mentor("Ammy","Bryan", 26, Gender.FEMALE));
 
         model.addAttribute("mentors", mentorList);
 
