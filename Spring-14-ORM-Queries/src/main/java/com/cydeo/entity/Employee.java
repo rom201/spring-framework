3e13430fb13cc26d72c2d0b2063930cbb80e8c6f
rom201 <kovalchukr1104@gmail.com>
2023-04-22 14:00:19 -0400

    queries , entities completed

---
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name ="employees")
+@Data
+@NoArgsConstructor
+public class Employee extends BaseEntity{
+
+//    first_name, last_name, email, hire_date, department, gender, salary, region_id
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer salary;
+
+//    private Department department;
+
+//    private Region region;
+
+
+
+}
