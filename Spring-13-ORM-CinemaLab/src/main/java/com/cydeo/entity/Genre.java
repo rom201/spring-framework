e4d3e6ac087c45d5d43c6cdaf1f4d45dfc97ed60
rom201 <kovalchukr1104@gmail.com>
2023-04-29 14:35:04 -0400

    updated all entities and complete derived query for account

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Genre.java
@@ -2,14 +2,17 @@ package com.cydeo.entity;
 
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Genre extends BaseEntity {
 
@@ -17,7 +20,10 @@ public class Genre extends BaseEntity {
     @ManyToMany(mappedBy = "genreLIst")
     private List<Movie> movieList;
 
-
-
-
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -2,7 +2,9 @@ package com.cydeo.entity;
 
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +13,8 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Ticket extends BaseEntity{
 
@@ -28,7 +31,12 @@ public class Ticket extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY)
     private User userAccount;
 
-
-
-
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "seatNumber=" + seatNumber +
+                ", rowNumber=" + rowNumber +
+                ", dateTime=" + dateTime +
+                '}';
+    }
 }
