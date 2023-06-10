db3e1a8807e07535ae837da92cdd39094d73471f
rom201 <kovalchukr1104@gmail.com>
2023-06-09 23:56:32 -0400

    two projects were added 19

---
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakOrganizationsAppApplication.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring19RestSecurityKeycloakOrganizationsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestSecurityKeycloakOrganizationsAppApplication.class, args);
+    }
+
+
+
+
+}
